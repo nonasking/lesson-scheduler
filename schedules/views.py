a6f47335e2487195d0486ee6f0d599744220a429
nonasking <nonasking@gmail.com>
2024-08-30 16:21:08 +0900

    커스텀 액션(대시보드) 추가

--- schedules/views.py
+++ schedules/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from .models import Teacher, Student, Schedule
 from .serializers import TeacherSerializer, StudentSerializer, ScheduleSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
+from django.utils import timezone
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
@@ -13,3 +17,15 @@ class StudentViewSet(viewsets.ModelViewSet):
 class ScheduleViewSet(viewsets.ModelViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
+
+    @action(detail=False, methods=['get'])
+    def dashboard(self, request):
+        year = request.query_params.get('year', timezone.now().year)
+        month = request.query_params.get('month', timezone.now().month)
+
+        schedules = Schedule.objects.filter(
+            scheduled_at__year=year,
+            scheduled_at__month=month,
+        ).values('scheduled_at').annotate(count=Count('id')).order_by('scheduled_at')
+
+        return Response({schedule['scheduled_at'].strftime('%Y-%m-%d'): schedule['count'] for schedule in schedules})
