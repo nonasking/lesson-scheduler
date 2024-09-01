02c9e313462d92cebcc23d0286c5796613b88232
nonasking <nonasking@gmail.com>
2024-09-01 16:43:13 +0900

    refactor(schedules/views.py,-schedules/models.py): API 로직 모듈화
    API 로직 모듈화

--- schedules/models.py
+++ schedules/models.py
@@ -1,3 +1,4 @@
+import stat
 from datetime import timedelta
 
 from django.db import models
@@ -43,17 +44,20 @@ class Schedule(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-    def mark_as_complete(self):
-        if self.is_complete:
-            raise ValidationError("Schedule is already completed.")
-        self.is_complete = True
-        self.completed_date = timezone.now().date()
-        self.save()
+    @classmethod
+    def create_schedule(cls, teacher_id, student_id, subject_id, scheduled_at):
+        if Schedule.objects.filter(
+            teacher_id=teacher_id, student_id=student_id, scheduled_at=scheduled_at
+        ).exists():
+            raise ValidationError("This schedule already exists.")
 
-    def delete_schedule(self):
-        if self.is_complete:
-            raise ValidationError("Completed schedules cannot be deleted.")
-        self.delete()
+        Schedule.objects.create(
+            teacher_id=teacher_id,
+            student_id=student_id,
+            subject_id=subject_id,
+            scheduled_at=scheduled_at,
+        )
+        return scheduled_at
 
     @classmethod
     def create_repeating_schedules(
@@ -97,6 +101,18 @@ class Schedule(models.Model):
                 created_schedules.append(current_date.date())
             current_date += delta
         return created_schedules
+
+    def mark_as_complete(self):
+        if self.is_complete:
+            raise ValidationError("Schedule is already completed.")
+        self.is_complete = True
+        self.completed_date = timezone.now().date()
+        self.save()
+
+    def delete_schedule(self):
+        if self.is_complete:
+            raise ValidationError("Completed schedules cannot be deleted.")
+        self.delete()
 
     class Meta:
         unique_together = ("teacher", "student", "scheduled_at")
--- schedules/views.py
+++ schedules/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from .models import Schedule, Teacher
 from .serializers import ScheduleSerializer
-from .utils import filter_by_teacher, filter_by_date_range, filter_by_completion_status
+from .utils import filter_by_completion_status, filter_by_date_range, filter_by_teacher
 
 
 def get_current_teacher(request):
@@ -20,26 +20,10 @@ def get_current_teacher(request):
     except Teacher.DoesNotExist:
         raise ValidationError({"error": "Invalid Teacher-ID."})
 
+
 class ScheduleViewSet(viewsets.ModelViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
-
-    def get_queryset(self):
-        teacher_id = self.request.query_params.get("teacher_id")
-        date_from = self.request.query_params.get("date_from")
-        date_to = self.request.query_params.get("date_to")
-        is_complete = self.request.query_params.get("is_complete")
-        
-        queryset = Schedule.objects.select_related('teacher', 'student', 'subject')
-
-        if teacher_id:
-            queryset = filter_by_teacher(queryset, teacher_id)
-        if date_from or date_to:
-            queryset = filter_by_date_range(queryset, date_from, date_to)
-        if is_complete is not None:
-            queryset = filter_by_completion_status(queryset, is_complete)
-
-        return queryset
 
     def create(self, request, *args, **kwargs):
         teacher_id = int(request.data.get("teacher_id"))
@@ -54,41 +38,62 @@ class ScheduleViewSet(viewsets.ModelViewSet):
                 {"error": "Permission denied"}, status=status.HTTP_403_FORBIDDEN
             )
 
-        if Schedule.objects.filter(
-            teacher_id=teacher_id, student_id=student_id, scheduled_at=scheduled_at
-        ).exists():
-            return Response(
-                {"error": "This schedule already exists."},
-                status=status.HTTP_400_BAD_REQUEST,
+        try:
+            created_schedule = Schedule.create_schedule(
+                teacher_id, student_id, subject_id, scheduled_at
             )
-
-        Schedule.objects.create(
-            teacher_id=teacher_id,
-            student_id=student_id,
-            subject_id=subject_id,
-            scheduled_at=scheduled_at,
-        )
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(
-            {"status": "Schedule created"},
+            {"status": "Schedule created", "date": created_schedule},
             status=status.HTTP_201_CREATED,
         )
 
-    def destroy(self, request, *args, **kwargs):
-        schedule = self.get_object()
+    @action(detail=False, methods=["post"], url_path="create-repeating")
+    def create_repeating(self, request):
+        teacher_id = int(request.data.get("teacher_id"))
+        student_id = int(request.data.get("student_id"))
+        start_date = request.data.get("start_date")
+        end_date = request.data.get("end_date")
+        frequency = int(request.data.get("frequency"))
 
         current_teacher = get_current_teacher(request)
-        if current_teacher != schedule.teacher:
+        current_teacher_id = current_teacher.id
+        subject_id = current_teacher.subject.id
+        if current_teacher_id != teacher_id:
             return Response(
                 {"error": "Permission denied"}, status=status.HTTP_403_FORBIDDEN
             )
 
         try:
-            schedule.delete_schedule()
+            created_schedules = Schedule.create_repeating_schedules(
+                teacher_id, student_id, subject_id, start_date, end_date, frequency
+            )
         except ValidationError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {"status": "Schedules created", "dates": created_schedules},
+            status=status.HTTP_201_CREATED,
+        )
+
+    def get_queryset(self):
+        teacher_id = self.request.query_params.get("teacher_id")
+        date_from = self.request.query_params.get("date_from")
+        date_to = self.request.query_params.get("date_to")
+        is_complete = self.request.query_params.get("is_complete")
+
+        queryset = Schedule.objects.select_related("teacher", "student", "subject")
+
+        if teacher_id:
+            queryset = filter_by_teacher(queryset, teacher_id)
+        if date_from or date_to:
+            queryset = filter_by_date_range(queryset, date_from, date_to)
+        if is_complete is not None:
+            queryset = filter_by_completion_status(queryset, is_complete)
+
+        return queryset
 
     @action(detail=False, methods=["get"])
     def dashboard(self, request):
@@ -131,30 +136,18 @@ class ScheduleViewSet(viewsets.ModelViewSet):
 
         return Response({"status": "Schedule marked as complete"})
 
-    @action(detail=False, methods=["post"], url_path="create-repeating")
-    def create_repeating(self, request):
-        teacher_id = int(request.data.get("teacher_id"))
-        student_id = int(request.data.get("student_id"))
-        start_date = request.data.get("start_date")
-        end_date = request.data.get("end_date")
-        frequency = int(request.data.get("frequency"))
+    def destroy(self, request, *args, **kwargs):
+        schedule = self.get_object()
 
         current_teacher = get_current_teacher(request)
-        current_teacher_id = current_teacher.id
-        subject_id = current_teacher.subject.id
-        if current_teacher_id != teacher_id:
+        if current_teacher != schedule.teacher:
             return Response(
                 {"error": "Permission denied"}, status=status.HTTP_403_FORBIDDEN
             )
 
         try:
-            created_schedules = Schedule.create_repeating_schedules(
-                teacher_id, student_id, subject_id, start_date, end_date, frequency
-            )
+            schedule.delete_schedule()
         except ValidationError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(
-            {"status": "Schedules created", "dates": created_schedules},
-            status=status.HTTP_201_CREATED,
-        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
